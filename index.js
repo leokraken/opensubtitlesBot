bf581801e8169b3b3e2160a488da869935c622c5
leokraken <joleocl@gmail.com>
2018-01-18 21:49:32 -0300

    Add text

--- index.js
+++ index.js
@@ -38,8 +38,5 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.onText(/\/help.*/, (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, 'Commands /help /echo arguments');
 });
